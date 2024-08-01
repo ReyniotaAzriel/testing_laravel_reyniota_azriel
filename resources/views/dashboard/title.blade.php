5199b233da2e7370f2c0e6ba8b022f5a4101f56c
ReyniotaAzriel <reyniotaa@gmail.com>
2024-08-02 04:14:44 +0700

    menambah dummy dashboard

---
+++ resources/views/dashboard/title.blade.php
@@ -0,0 +1,17 @@
+<div class="app-content-header">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <h3 class="display-5 fw-bolder">Welcome!</h3>
+                <h5 class="mb-0">Azriel RT Dashboard</h5>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-end">
+                    <li class="breadcrumb-item active" aria-current="page">
+                        Home
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
